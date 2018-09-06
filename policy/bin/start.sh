a7c95bef0874702e7d1f829f3d95bac85ea15e4b
Joe Garcia <joe@joe-garcia.com>
2018-09-06 18:13:03 -0400

    Cleanup

--- policy/bin/start.sh
+++
@@ -1,2 +0,0 @@
-bin/0_install_systems.sh && bin/1_insecure_demo.sh && bin/2_secure_demo.sh && bin/print_login.sh
-
--- policy/gitlab/InsecureDemo/callJenkins.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X POST --user JENKINS_USER:${INSECURE_JENKINS_PASS} --header "JOB-TOKEN: $CI_JOB_TOKEN" "JENKINS_URL/job/InsecureDemo/build"
